7d958070414fce594169b06014758defc98469d3
Om Prakash Mallik <opm202000@gmail.com>
2024-03-29 18:23:33 +0530

    its final

--- src/Contact/Contanct.js
+++ src/Contact/Contanct.js
@@ -6,7 +6,7 @@ function Contact() {
         <div className='flex flex-col lg:flex-row w-full'>
             <div className='lg:w-1/2'>
                 <img
-                    className='w-full sticky top-0 h-screen object-cover'
+                    className='w-full sticky top-0'
                     src={avatar}
                     alt='profile-pic'
                 />
